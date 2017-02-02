adc18fcf80e8d856d1603bc4ad75f1fdf4dbe4ea
gamedevelopersbrazil <gamedevelopersbrazil@gmail.com>
2017-02-02 17:30:51 -0200

    Primeira Postagem
    Projeto de game para o Big Hackathon da Campus Party 2017 (Objetivos
    Sustentáveis).

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainCamera : MonoBehaviour {
+
+    public GameObject screen1;
+    public GameObject screen2;
+    public float waiting;
+    private void Start()
+    {
+        screen1.SetActive(true);
+        screen2.SetActive(false);
+    }
+
+    private void Update()
+    {
+        waiting += Time.deltaTime;
+        Debug.Log("Valor" + waiting);
+        if(waiting >= 2.5)
+        {
+            screen1.SetActive(false);
+            screen2.SetActive(true);
+        }
+
+        if(waiting >= 5)
+        {
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+        }
+    }
+}
